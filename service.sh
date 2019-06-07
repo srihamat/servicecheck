c437eb875bb400cb7a9e522271a95e8f0c578444
srihamat <36180751+srihamat@users.noreply.github.com>
2019-06-07 09:31:36 +0700

    Update variable token and more comment

--- service.sh
+++ service.sh
@@ -6,14 +6,18 @@
 
 err_flag=0
 err_service=none
+#ens192 is our example interface name we interest, please update yours
 servIP=`ip addr show ens192 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'`
-token=[your line notification token]
+#ZquoMHIKoNjdUCp8fNxVCbTETirZAQkJbWWmb7e1234 is out example token we have, pls get yours from https://notify-bot.line.me
+token=ZquoMHIKoNjdUCp8fNxVCbTETirZAQkJbWWmb7e1234
 
 #grep problem
 err_previously=$(cat linelog.txt | grep 'problem')
 
 rm $HOME/linelog.txt
 
+# nginx and mysqld are our example services we interest, please update yours
+# Modify services you interest, example replace mysqld with another db, replace nginx with another web service
 for SERVICE in nginx mysqld
 do
 	if pgrep -x "$SERVICE" >/dev/null
@@ -26,21 +30,22 @@ do
 	fi
 done
 
+# Sending line message to let administrator know before customer
 if [ $err_flag -eq 1 ];
 then
 	echo "server is having problem" >> $HOME/linelog.txt
 	# if problem found previorsly
 	# no need to report server is having problem again
 	if [ "$err_previously" != "server is having problem" ]; then
-	    curl -X POST -H 'Authorization: Bearer $token' -F 'message=Server "'"$servIP"'" has a problem with "'"$err_service"'"
+	    curl -X POST -H 'Authorization: Bearer '$token -F 'message=Server "'"$servIP"'" has a problem with "'"$err_service"'"
             ' https://notify-api.line.me/api/notify.
 	fi;
 else
 	echo "server is working fine !" >> $HOME/linelog.txt
 	# if problem found previorsly
-	# report server is working fine (via line)
+	# report server is working fine
 	if [ "$err_previously" = "server is having problem" ]; then
-	    curl -X POST -H 'Authorization: Bearer $token' -F 'message=Server "'"$servIP"'" is working fine !"
+	    curl -X POST -H 'Authorization: Bearer '$token -F 'message=Server "'"$servIP"'" is working fine !"
 	    ' https://notify-api.line.me/api/notify.
 	fi;
 fi
